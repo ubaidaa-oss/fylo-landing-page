049938e97b71c2881aa4e7cfb6e0ff7aad411199
ubaid <ubaidrashid141@gmail.com>
2024-07-11 16:48:51 +0500

    Add files via upload
    ok project

---
+++ fylo-landing-page/js/index.js
@@ -0,0 +1,21 @@
+let form = document.querySelector('#myForm');
+let submit = document.querySelector('#submit');
+
+submit.value = 'Get Started'
+
+let form2 = document.querySelector('#mySecondForm');
+let submit2= document.querySelector('#submitSecond');
+
+form.addEventListener('submit', function(event){
+    event.preventDefault()
+let email = document.querySelector('#email').value;
+
+
+
+})
+
+form2.addEventListener('submit',function(event){
+    event.preventDefault()
+})
+
+submit2.value = 'Get Started For Free'
